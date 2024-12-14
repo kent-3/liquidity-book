903ee1ed564f90bc69b91bd04ef60295e0c52123
kent-3 <100624004+kent-3@users.noreply.github.com>
2024-12-13 20:39:10 -0500

    Revert "experiment putting everything in one file"
    This reverts commit 89059ce3567240ead375a8231111506efb16e05c.

---
+++ contracts/lb_quoter/src/contract.rs
@@ -0,0 +1,50 @@
+use crate::{prelude::*, query::*, state::*};
+use cosmwasm_std::{entry_point, to_binary, Binary, Deps, DepsMut, Env, MessageInfo};
+use lb_interfaces::lb_quoter::*;
+
+#[entry_point]
+pub fn instantiate(deps: DepsMut, env: Env, info: MessageInfo, msg: InstantiateMsg) -> Result<()> {
+    let factory_v2_2 = msg
+        .factory_v2_2
+        .map(|raw_contract| raw_contract.valid(deps.api))
+        .transpose()?;
+
+    let router_v2_2 = msg
+        .router_v2_2
+        .map(|raw_contract| raw_contract.valid(deps.api))
+        .transpose()?;
+
+    FACTORY_V2_2.save(deps.storage, &factory_v2_2)?;
+    ROUTER_V2_2.save(deps.storage, &router_v2_2)?;
+
+    Ok(())
+}
+
+// TODO: see what happens if I remove this
+#[entry_point]
+pub fn execute(deps: DepsMut, env: Env, info: MessageInfo, msg: ExecuteMsg) -> Result<()> {
+    unimplemented!()
+}
+
+#[entry_point]
+pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> Result<Binary> {
+    match msg {
+        QueryMsg::GetFactoryV2_2 {} => {
+            let factory_v2_2 = FACTORY_V2_2.load(deps.storage)?;
+            let response = FactoryV2_2Response { factory_v2_2 };
+            to_binary(&response)
+        }
+        QueryMsg::GetRouterV2_2 {} => {
+            let router_v2_2 = ROUTER_V2_2.load(deps.storage)?;
+            let response = RouterV2_2Response { router_v2_2 };
+            to_binary(&response)
+        }
+        QueryMsg::FindBestPathFromAmountIn { route, amount_in } => {
+            to_binary(&find_best_path_from_amount_in(deps, route, amount_in)?)
+        }
+        QueryMsg::FindBestPathFromAmountOut { route, amount_out } => {
+            to_binary(&find_best_path_from_amount_out(deps, route, amount_out)?)
+        }
+    }
+    .map_err(Error::CwErr)
+}
---
+++ contracts/lb_quoter/src/error.rs
@@ -0,0 +1,22 @@
+//! ### Custom Errors for LB_Pair contract.
+
+use cosmwasm_std::StdError;
+use lb_libraries::math::{u128x128_math::U128x128MathError, u256x256_math::U256x256MathError};
+
+#[derive(thiserror::Error, Debug)]
+pub enum LbQuoterError {
+    // Generic Errors
+    #[error("Generic {0}")]
+    Generic(String),
+
+    #[error("InvalidLength")]
+    InvalidLength,
+
+    // Error Wrappings from Dependencies
+    #[error(transparent)]
+    CwErr(#[from] StdError),
+    #[error(transparent)]
+    U128Err(#[from] U128x128MathError),
+    #[error(transparent)]
+    U256Err(#[from] U256x256MathError),
+}
